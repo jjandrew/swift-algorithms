c6c48343772af65d6704bf3821d1691d834cfd2d
JJ Andrew <andrew.j@etoncollege.org.uk>
2019-11-05 12:18:03 +0000

    initial commit of project files

---
+++ standardAlgorithms/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  standardAlgorithms
+//
+//  Created by Andrew, James (NA) on 05/11/2019.
+//  Copyright © 2019 Andrew, James (NA). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let name = "JJ"
+
+for i in 1...3 {
+    print(i)
+}
+
+print(name)
